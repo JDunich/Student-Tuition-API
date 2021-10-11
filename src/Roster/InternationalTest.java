ae8184153874cb8c864f6aabb357e75de77d3e1f
Jack Dunich <jadunich@gmail.com>
2021-10-11 16:06:16 -0400

    Update Files

--- src/Roster/InternationalTest.java
+++ src/Roster/InternationalTest.java
@@ -6,7 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InternationalTest {
 
+    International student;
+    Profile info = new Profile("John", Major.CS);
+
     @Test
-    void tuitionDue() {
+    void tuitionDueNoAbroad() {
+        student = new International(info, 12, false);
+        student.tuitionDue();
+        assertEquals(35655, student.getTuitionDue());
     }
+
+    @Test
+    void tuitionDueAbroad() {
+        student = new International(info, 12, true);
+        student.tuitionDue();
+        assertEquals(5918, student.getTuitionDue());
+    }
+
 }
--- src/Roster/RosterTest.java
+++ src/Roster/RosterTest.java
@@ -1,14 +1,106 @@
 package Roster;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RosterTest {
 
-    @org.junit.jupiter.api.Test
-    void add() {
+    Roster arr = new Roster();
+
+    @Test
+    void addStudent() {
+        Profile info = new Profile("John", Major.CS);
+        Student student = new Student(info, 10);
+        assertEquals(true, arr.add(student));
     }
 
-    @org.junit.jupiter.api.Test
-    void remove() {
+    @Test
+    void addResident() {
+        Profile info = new Profile("John", Major.CS);
+        Resident student = new Resident(info, 10);
+        assertEquals(true, arr.add(student));
     }
+
+    @Test
+    void addNonResident() {
+        Profile info = new Profile("John", Major.CS);
+        NonResident student = new NonResident(info, 10);
+        assertEquals(true, arr.add(student));
+    }
+
+    @Test
+    void addTriState() {
+        Profile info = new Profile("John", Major.CS);
+        TriState student = new TriState(info, 10, "NY");
+        assertEquals(true, arr.add(student));
+    }
+
+    @Test
+    void addInternational() {
+        Profile info = new Profile("John", Major.CS);
+        International student = new International(info, 10, false);
+        assertEquals(true, arr.add(student));
+    }
+
+    @Test
+    void addSame() {
+        Profile info = new Profile("John", Major.CS);
+        Student student1 = new Student(info, 10);
+        Student student2 = new Student(info, 10);
+        arr.add(student1);
+        assertEquals(false, arr.add(student2));
+    }
+
+    @Test
+    void addFive() {
+        Profile info1 = new Profile("John1", Major.CS);
+        Profile info2 = new Profile("John2", Major.CS);
+        Profile info3 = new Profile("John3", Major.CS);
+        Profile info4 = new Profile("John4", Major.CS);
+        Profile info5 = new Profile("John5", Major.CS);
+        Student student1 = new Student(info1, 10);
+        Student student2 = new Student(info2, 10);
+        Student student3 = new Student(info3, 10);
+        Student student4 = new Student(info4, 10);
+        Student student5 = new Student(info5, 10);
+        arr.add(student1);
+        arr.add(student2);
+        arr.add(student3);
+        arr.add(student4);
+        assertEquals(true, arr.add(student5));
+    }
+
+    @Test
+    void removeStudent() {
+        Profile info = new Profile("John", Major.CS);
+        Student student = new Student(info, 10);
+        arr.add(student);
+        assertEquals(true, arr.remove(student));
+    }
+
+    @Test
+    void removeResident() {
+        Profile info = new Profile("John", Major.CS);
+        Resident student = new Resident(info, 10);
+        arr.add(student);
+        assertEquals(true, arr.remove(student));
+    }
+
+    @Test
+    void removeNonResident() {
+        Profile info = new Profile("John", Major.CS);
+        NonResident student = new NonResident(info, 10);
+        arr.add(student);
+        assertEquals(true, arr.remove(student));
+    }
+
+    @Test
+    void removeStudentNotIn() {
+        Profile info = new Profile("John", Major.CS);
+        Student student = new Student(info, 10);
+        assertEquals(false, arr.remove(student));
+    }
+
+
 }
